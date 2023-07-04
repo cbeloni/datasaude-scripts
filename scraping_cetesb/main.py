f310f100c3a57fbba79b81c81750775c1b86557a
Cauê Beloni <cbeloni@gmail.com>
2023-07-03 21:07:01 -0300

    melhoria customização logs
    add .gitignore
    
    scrapping cetesb

--- scraping_cetesb/main.py
+++ scraping_cetesb/main.py
@@ -1,12 +1,20 @@
 from core.log_config import Log
-from core.database import criar_conexao
-
-
+from scraping_cetesb.api import CestebConsuilta
 
 _log = Log("scrapping_cetesb")
 
+
+
+
 if __name__ == '__main__':
     _log.info("Starting scrapping cetesb")
-    print('main scrapping')
-    # conexao = criar_conexao()
-    # _log.info(f"conectado: {conexao.is_connected()}")
+    cestebConsuilta = CestebConsuilta()
+    cookie = cestebConsuilta.autenticacao()
+    _log.info(f"autenticado: {cookie}")
+
+    filtros = cestebConsuilta.filtrar()
+    _log.info(f"realizado filtros com sucesso")
+
+    cestebConsuilta.scrap()
+    _log.info(f"Consulta realizada com sucesso!")
+
