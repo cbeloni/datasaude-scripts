0d0ed254817799495bdc910c090252ea03bffd36
Cauê Beloni <cbeloni@gmail.com>
2023-07-13 21:24:10 -0300

    carga csv to sql 2022

--- csv_to_sql/main_poluente_historico_csv_to_sql.py
+++ csv_to_sql/main_poluente_historico_csv_to_sql.py
@@ -1,3 +1,4 @@
+from core.poluente_repository import get_poluente_scrap_pendentes
 from poluente_historico_csv_to_sql import csv_to_sql
 from core.log_config import Log
 
@@ -6,8 +7,16 @@ _log = Log("main_poluente_historico_csv_to_sql")
 path = "/home/caue/Documentos/FILES_CETESB/2022_csv"
 
 if __name__ == '__main__':
-    file = '01012022_01012023_63_63.html.csv'
-    path_file = f"{path}/{file}"
     # csv_to_sql()
-    _log.info(f'convertendo: {path_file}')
+
+    resultados = get_poluente_scrap_pendentes('2022-01-01 00:00:00', 'PARSED')
+    for resultado in resultados:
+        id, i_rede, data_inicial, data_final, i_tipo_dado, estacao, parametro, created_at, updated_at, file = resultado
+
+        path_file = f"{path}/{file}.csv"
+        _log.info(f'salvando: {path_file}')
+        try:
+            csv_to_sql(path_file)
+        except Exception as ex:
+            _log.error(f'{path_file} - Error: {ex}')
 
