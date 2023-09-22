48e9712bbcd1ab879f6cb3f6c2c95a20463db873
Cauê Beloni <cbeloni@gmail.com>
2023-09-22 19:43:36 -0300

    refatoração

--- interpolacao/main.py
+++ interpolacao/main.py
@@ -9,60 +9,23 @@ import rasterio
 from rasterio import features
 from rasterio.transform import from_origin
 
-readings = pd.read_csv('amostra/amostra_2023_mp10.csv', index_col='id')
-canvas = gpd.read_file('contorno/hexgrid_v2.shp')
-canvas['points'] = canvas.centroid
 
-km_c = 10**3
-STEP_SIZE = km_c * 40
-MAX_RANGE = km_c * 400
-NEIGHBOURS = 8
-NUMBER_OF_RANGES = 16
-POWER = 3
-READING = 'media_mp10'
+def ler_fontes(amostra_file, contorno_file):
+    readings = pd.read_csv(amostra_file, index_col='id')
+    canvas = gpd.read_file(contorno_file)
+    canvas['points'] = canvas.centroid
+    return readings, canvas
 
-sample = readings[['x', 'y', READING]]
-arr = sample.dropna().values
-arr[:3]
-
-geometries = gpd.points_from_xy(x=arr[:, 0], y=arr[:, 1])
-garr = gpd.GeoDataFrame(arr[:, -1])
-garr['geometry'] = geometries
-garr.head()
-
-fig, ax = plt.subplots(figsize=(10, 10))
-garr.set_geometry = 'geometry'
-garr.plot(ax=ax, column=0, legend=False, vmin=10, vmax=50, cmap='coolwarm')
-#ax.set_title('2021-01-01 Daily PM 2.5 concentrations in Poland')
-#plt.show()
-
+def plotar_amostra(garr):
+    garr.set_geometry = 'geometry'
+    garr.plot(ax=ax, column=0, legend=False, vmin=10, vmax=50, cmap='coolwarm')
+    plt.show()
 
 def idw_apply(x, known, nn=-1, power=1):
     pred = inverse_distance_weighting(known, np.array([x.x, x.y]), nn, power)
     return pd.Series([x, pred])
-# Predict
-predicted = canvas['points'].apply(idw_apply, known=arr, power=POWER)
-predicted.columns = ['coordinates', 'yhat']
 
-colors = ['#008000', '#FFFF00', '#FFA500', '#FF0000', '#800080']
-cmap = ListedColormap(colors)
-
-# Merge with canvas
-df = canvas.join(predicted)
-df = df[['geometry', 'yhat']]
-ax.set_axis_off()
-ax = df.plot(column='yhat', legend=False, vmin=10, vmax=50, figsize=(10, 10), cmap=cmap, ax=ax)
-
-plt.tight_layout()
-plt.show()
-# output_file = "output/shapefile"
-# df.to_file(output_file)
-
-geo_json_output_path = "output/mapa_poluentes.geojson"
-df.to_file(geo_json_output_path, driver="GeoJSON")
-
-output_png_path = 'output/mapa_poluente_mp_10.png'
-def gera_png():
+def gera_png(geo_json_output_path):
     gdf = gpd.read_file(geo_json_output_path)
     colors = ['#008000', '#FFFF00', '#FFA500', '#FF0000', '#800080']
     cmap = ListedColormap(colors)
@@ -74,11 +37,8 @@ def gera_png():
     plt.tight_layout()
     plt.savefig(output_png_path, dpi=300, bbox_inches='tight', pad_inches=0.1)
     print(f'Imagem salva em {output_png_path}')
-# salvar em png a partir do geojson
-gera_png()
 
-output_filename_transparente = 'output/mapa_poluente_mp_10_transparente.png'
-def gera_transparencia():
+def gera_transparencia(output_filename_transparente):
     imagem_png = Image.open(output_png_path)
 
     transparencia = 255  # # transparência restante em 100%
@@ -88,8 +48,6 @@ def gera_transparencia():
 
     imagem_png.save(output_filename_transparente)
     imagem_png.close()
-
-gera_transparencia()
 
 def gera_geotif():
     # Lê o arquivo GeoJSON
@@ -126,3 +84,53 @@ def gera_geotif():
 
     plt.show()
 
+def interpolar(amostra_file, contorno_file):
+    readings, canvas = ler_fontes(amostra_file, contorno_file)
+
+    km_c = 10 ** 3
+    POWER = 3
+    READING = 'media_mp10'
+    sample = readings[['x', 'y', READING]]
+    arr = sample.dropna().values
+    arr[:3]
+
+    geometries = gpd.points_from_xy(x=arr[:, 0], y=arr[:, 1])
+    garr = gpd.GeoDataFrame(arr[:, -1])
+    garr['geometry'] = geometries
+    garr.head()
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # plotar_amostra(garr)
+
+    # Predict
+    predicted = canvas['points'].apply(idw_apply, known=arr, power=POWER)
+    predicted.columns = ['coordinates', 'yhat']
+
+    colors = ['#008000', '#FFFF00', '#FFA500', '#FF0000', '#800080']
+    cmap = ListedColormap(colors)
+
+    # Merge with canvas
+    df = canvas.join(predicted)
+    df = df[['geometry', 'yhat']]
+    ax.set_axis_off()
+    ax = df.plot(column='yhat', legend=False, vmin=10, vmax=50, figsize=(10, 10), cmap=cmap, ax=ax)
+
+    plt.tight_layout()
+    plt.show()
+    # output_file = "output/shapefile"
+    # df.to_file(output_file)
+    return df
+
+
+if __name__ == '__main__':
+    df = interpolar('amostra/amostra_2023_mp10.csv', 'contorno/hexgrid_v2.shp')
+
+    geo_json_output_path = "output/mapa_poluentes.geojson"
+    df.to_file(geo_json_output_path, driver="GeoJSON")
+
+    output_png_path = 'output/mapa_poluente_mp_10.png'
+    gera_png(geo_json_output_path)
+
+    output_filename_transparente = 'output/mapa_poluente_mp_10_transparente.png'
+    gera_transparencia(output_filename_transparente)
