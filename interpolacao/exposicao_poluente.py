8fe0128f1c5fd6c439add2260f67620c0d00166c
Cauê Beloni <cbeloni@gmail.com>
2023-09-24 10:26:03 -0300

    nome campo variável

--- interpolacao/exposicao_poluente.py
+++ interpolacao/exposicao_poluente.py
@@ -5,14 +5,14 @@ import geopandas as gpd
 
 _log = Log("exposicao_poluente")
 
-def indice_poluente_por_utm(x, y, arquivo_geojson):
+def indice_poluente_por_utm(x, y, arquivo_geojson, campo):
     point_utm = Point(x, y)
 
     gdf = gpd.read_file(arquivo_geojson)
     result = gdf.contains(point_utm)
 
     if result.any():
-        return gdf.loc[result, "yhat"].values[0]
+        return gdf.loc[result, campo].values[0]
 
     _log.error("O ponto UTM não está dentro do polígono.")
     return 0
@@ -22,5 +22,5 @@ if __name__ == '__main__':
     data = '20220101'
     nome_arquivo = f'poluente_{poluente}_{data}'
     arquivo_geojson = f'output/{nome_arquivo}.geojson'
-    indice = indice_poluente_por_utm(324480.5080984961, 7383606.510285771, arquivo_geojson)
+    indice = indice_poluente_por_utm(324480.5080984961, 7383606.510285771, arquivo_geojson, "yhat")
     _log.info(f"O valor de exposição do poluente para o ponto UTM é: {indice}")
