0494bb876874fd5dca8c158a7bf88b337b438bf0
Cauê Beloni <cbeloni@gmail.com>
2023-09-24 17:12:45 -0300

    fix: UTM estações

--- geolocalizacao/main.py
+++ geolocalizacao/main.py
@@ -2,6 +2,7 @@ import requests
 import time
 from estacao_address import addresses
 from dotenv import load_dotenv, dotenv_values
+import pyproj
 
 load_dotenv()
 _config = dotenv_values("../.env")
@@ -17,9 +18,19 @@ def get_latitude_longetitude(address):
     latitude = location['lat']
     longitude = location['lng']
     return (latitude, longitude)
+def converte_coordenadas_UTM(latitude, longitude):
+    input_crs = pyproj.CRS.from_epsg(4326)
+    output_crs = pyproj.CRS.from_epsg(29193)
+
+    transformer = pyproj.Transformer.from_crs(input_crs, output_crs, always_xy=True)
+
+    # Converta de latitude e longitude para UTM
+    return transformer.transform(longitude, latitude)
+
 
 if __name__ == '__main__':
     for estacao in addresses:
         latitude, longitude = get_latitude_longetitude(estacao)
-        print(f'{estacao};{latitude};{longitude}')
+        x, y = converte_coordenadas_UTM(latitude, longitude)
+        print(f'{estacao};{latitude};{longitude};{x};{y}')
         time.sleep(2)
