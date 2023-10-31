123b6d78130bc72f11f314656adec55ea49fb5db
Cauê Beloni <cbeloni@gmail.com>
2023-10-30 22:41:32 -0300

    envio arquivos bucket

--- boto3-client/gestor_arquivos.py
+++ boto3-client/gestor_arquivos.py
@@ -23,6 +23,8 @@ def listar(bucket_name: str):
     if 'Contents' in response:
         for obj in response['Contents']:
             print(obj['Key'])
+            if "1_Krig_media_mp10_2_1" == obj['Key'] or "1_Krig_media_mp10_2_1.png" == obj['Key']:
+                continue
             remover('maps-pub', obj['Key'])
     else:
         print('O bucket está vazio.')
@@ -35,9 +37,23 @@ def remover(bucket_name: str, key: str):
 
 def query_poluente_plot():
     return """
-        select arquivo_escala_fixa_png, arquivo_escala_movel_png 
-         from poluente_plot;
+        select id, arquivo_escala_fixa_png, arquivo_escala_movel_png 
+         from poluente_plot
+         where bucket_movel is null or bucket_fixa is null;
     """
+
+update_poluente_plot = """
+    UPDATE poluente_plot 
+    set bucket_fixa = %s, bucket_movel = %s  
+    where id = %s;
+"""
+
+def encurta_nome(file_name):
+    path, file_name = file_name.rsplit('/', 1)
+    file_parts = file_name.split("_")
+    new_file_name = "_".join(file_parts[:2]) + "." + file_name.split(".")[-1]
+    new_file_path = path + "/" + new_file_name
+    return new_file_path
 
 if __name__ == '__main__':
     _log.info("Criando conexão")
@@ -48,16 +64,22 @@ if __name__ == '__main__':
     cursor.execute(query_poluente_plot())
 
     for row in cursor.fetchall():
-        arquivo_escala_fixa_png, arquivo_escala_movel_png = row
+        id, arquivo_escala_fixa_png, arquivo_escala_movel_png = row
+        arquivo_escala_fixa_png_short = encurta_nome(arquivo_escala_fixa_png)
+        arquivo_escala_movel_png_short = encurta_nome(arquivo_escala_movel_png)
         enviar('maps-pub',
                f'{_PATH_VOLUME}{arquivo_escala_fixa_png}',
-               arquivo_escala_fixa_png,
+               arquivo_escala_fixa_png_short,
                'image/png')
 
         enviar('maps-pub',
                f'{_PATH_VOLUME}{arquivo_escala_movel_png}',
-               arquivo_escala_movel_png,
+               arquivo_escala_movel_png_short,
                'image/png')
+
+        params = (str(arquivo_escala_fixa_png_short), str(arquivo_escala_movel_png_short), str(id))
+        cursor.execute(update_poluente_plot, params)
+        conexao.commit()
 
     cursor.close()
     conexao.close()
